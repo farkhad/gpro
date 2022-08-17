d01a5ddab3ed29f1ad8eadf9fc9c2719f3060806
farkhad <farkhad@gmail.com>
2022-08-17 11:51:41 +0600

    Margin on Home

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $marketFiles = array_slice($marketFiles, 0, 3);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 
-<body class="m-3">
+<body class="m-5">
 <?php
 $page = pathinfo(__FILE__, PATHINFO_FILENAME);
 include 'nav.php';
