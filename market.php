9f620d9797d90846b42d0cda9a19143a80aa8df1
farkhad <farkhad@gmail.com>
2022-08-17 11:55:14 +0600

    Margin on Market/RaceAnalysis

--- market.php
+++ market.php
@@ -42,7 +42,7 @@ $message = "\nMarket file has been stored under <b>$gzfilename</b>\n";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 
-<body class="m-3">
+<body class="m-5">
 <?php
 $page = pathinfo(__FILE__, PATHINFO_FILENAME);
 include 'nav.php';
--- postrace.php
+++ postrace.php
@@ -60,7 +60,7 @@ if (false !== preg_match($pattern, $postraceHtml, $matches)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 
-<body class="m-3">
+<body class="m-5">
 <?php
 $page = pathinfo(__FILE__, PATHINFO_FILENAME);
 include 'nav.php';
