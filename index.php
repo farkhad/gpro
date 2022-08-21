2897576b90c1e88cc6c3cb05354f96068324c42c
farkhad <farkhad@gmail.com>
2022-08-21 19:48:56 +0600

    icons

--- index.php
+++ index.php
@@ -30,8 +30,10 @@ $marketFiles = array_slice($marketFiles, 0, 3);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GPRO Home Server</title>
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script async src="https://kit.fontawesome.com/f711a4bfbd.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="m-5">
--- nav.php
+++ nav.php
@@ -12,16 +12,16 @@ function linkAttr($page, $currentPage)
 ?>
 <ul class="nav nav-tabs mb-3">
     <li class="nav-item">
-        <a <?= linkAttr('index', $page) ?>>Home</a>
+        <a <?= linkAttr('index', $page) ?>><i class="fa-solid fa-house"></i> Home</a>
     </li>
     <li class="nav-item">
-        <a <?= linkAttr('find', $page) ?>>Find Best Driver</a>
+        <a <?= linkAttr('find', $page) ?>><i class="fa-solid fa-magnifying-glass"></i> Find Best Driver</a>
     </li>
     <li class="nav-item">
-        <a <?= linkAttr('profile', $page) ?>>Driver's Profile</a>
+        <a <?= linkAttr('profile', $page) ?>><i class="fa-solid fa-user"></i> Driver's Profile</a>
     </li>
     <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Download</a>
+        <a class="nav-link <?= in_array($page, ['postrace', 'market']) ? 'active' : '' ?> dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><i class="fa-solid fa-download"></i> Download</a>
         <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="postrace.php">Latest Race Analysis</a></li>
             <li><a class="dropdown-item" href="market.php">Latest Market Database</a></li>
