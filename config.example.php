53918d1147c227aa6b26337d694c6c389f91fe0d
farkhad <farkhad@gmail.com>
2024-01-25 20:57:21 +0600

    Install SleekDB

--- config.example.php
+++ config.example.php
@@ -6,10 +6,11 @@ const PASSWORD = "limited_password";
 const GPRO_URL = 'https://www.gpro.net/gb/';
 const HASH = '68c6e8ddaddcfa57a59a6b08c43fe477';
 const GPRO_UA = 'GPRO Home Server';
+const DB_FOLDER_NAME = 'database';
 
 define('ACCOUNTS', [
-    \FOLDER_NAME => [
-        'username' => \USERNAME,
-        'password' => \PASSWORD,
+    FOLDER_NAME => [
+        'username' => USERNAME,
+        'password' => PASSWORD,
     ],
 ]);
